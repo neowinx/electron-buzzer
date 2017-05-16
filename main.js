87ae3d9afbfda8b1cadf9463111b05d02b026656
Pedro Flores <pflores@dncp.gov.py>
2017-05-16 16:19:47 -0400

    multicast networking functionality added

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const Menu = electron.Menu
 const Tray = electron.Tray
 const dialog = electron.dialog
 
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
+const client = dgram.createSocket('udp4');
+
 let appIcon = null
 
 function putInTray() {
@@ -21,7 +25,7 @@ function putInTray() {
   appIcon.setToolTip('Buzz me!')
   appIcon.setContextMenu(contextMenu)
   appIcon.on('click', () => {
-    dialog.showErrorBox('Buzzz', 'BZZZZB! BZZZB! BZZZB! BZZZB! BZZZB!')
+    client.send(Buffer.from('Beep! Beep! Beep! Beep!'), 41234)
   })
 }
 
@@ -31,3 +35,35 @@ function removeTray() {
 }
 
 app.on('ready', putInTray)
+
+client.on('listening', function () {
+  var address = client.address();
+  console.log('client listening on ' + address.address + ":" + address.port);
+  client.setBroadcast(true)
+  client.setMulticastTTL(128);
+  client.addMembership('230.185.192.108');
+});
+
+socket.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  socket.close();
+});
+
+socket.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Buzzz',
+    message: msg.toString()
+  })
+});
+
+socket.on('listening', () => {
+  var address = socket.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+  socket.setBroadcast(true)
+  socket.setMulticastTTL(128);
+  socket.addMembership('230.185.192.108');
+});
+
+socket.bind(41234);
